cde416280b6531b986b6b8bbb1a6889a16230851
Chris Hays <john.hays@yale.edu>
2020-03-25 00:33:18 -0400

    carlo's changes to datasets.py

--- datasets.py
+++ datasets.py
@@ -7,25 +7,26 @@ import torch
 import glob
 
 class CapIter:
-    def __init__(self, cap, n_frames):
+    def __init__(self, cap, n_frames=None):
         self.cap = cap
         self.n_frames = n_frames
         self.i = 0
     def __iter__(self):
         return self
     def __next__(self):
+        if self.n_frames != None and self.i == self.n_frames:
+            raise StopIteration
         ok, frame = self.cap.read()
-        if (ok and self.i < self.n_frames):
+        if ok:
             self.i += 1
             return frame
-        else:
-            raise StopIteration
 
 class DeepfakeDataset(torch.utils.data.Dataset):
-    def __init__(self, folders, n_frames=float("inf"), train=True):
+    def __init__(self, folders, n_frames=None, train=True, device=None):
         self.n_frames = n_frames
         self.videos = []
         self.train = train
+        self.device = device
         for folder in folders:
             if (train):
                 with open(os.path.join(folder, 'metadata.json')) as f:
@@ -33,10 +34,13 @@ class DeepfakeDataset(torch.utils.data.Dataset):
                     videos = [(os.path.join(folder, video), metadata) for (video, metadata) in videos.items()]
                     self.videos += videos
             else:
-                self.videos = glob.glob(folder+"/*")
+                self.videos += glob.glob(folder+"/*")
     def __process_frame(self, frame):
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = torch.tensor(frame)
+        frame = cv2.UMat(frame)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
+        frame = torch.tensor(cv2.UMat.get(frame))
+        if self.device:
+            frame.to(self.device)
         frame = frame.permute(2, 0, 1)
         frame = frame / 255.
         return frame
@@ -50,7 +54,8 @@ class DeepfakeDataset(torch.utils.data.Dataset):
         frames = list(map(self.__process_frame, it))
         cap.release()
         if (self.train):
-            return (torch.stack(frames), metadata['label'])
+            metadata['path'] = video
+            return (torch.stack(frames), metadata)
         else:
             return torch.stack(frames)
     def __len__(self):
